80fb6b0b174ad28d3f267a6aae4eaa95d39227d4
NachoSaave <b12josito@gmail.com>
2026-05-19 10:48:37 -0400

    fiux

--- src/main/java/com/example/notificacion/Controller/NotificacionController.java
+++ src/main/java/com/example/notificacion/Controller/NotificacionController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +28,7 @@ public class NotificacionController {
     }
 
     @PostMapping
-    private Notificacion guardarNoti(Notificacion noti){
+    private Notificacion guardarNoti(@RequestBody Notificacion noti){
         return serv.guardarNoti(noti);
     }
 
@@ -35,12 +37,12 @@ public class NotificacionController {
         return serv.buscarNotiPorId(id);
     }
 
-    @PutMapping
-    private Notificacion actualizarNoti(Notificacion noti){
+    @PutMapping("/{id}")
+    private Notificacion actualizarNoti(@PathVariable Long id, @RequestBody Notificacion noti){
         return serv.actualizarNoti(noti);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     private void eliminarNoti(Notificacion noti){
         serv.eliminarNoti(noti);
     }
